e45f165b01969af1a343ba40bbcbdf19229ef0db
Mooradal <moorad2003@gmail.com>
2018-12-16 22:15:37 +0000

    fixed minor problems

--- index.js
+++ index.js
@@ -23,6 +23,15 @@ app.listen(process.env.PORT || 4000, () => {
 });
 
 app.use(cors());
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 app.set('views', path.join(__dirname, 'Client/Pug'));
 app.set('view engine', 'pug');
 
